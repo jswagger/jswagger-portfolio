a97ad410e7d6450be0ca62481192c1275e2ac357
Jeremy Swagger <jswagger@agvance.net>
2026-07-27 15:16:18 -0500

    Add basic personalization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import placeholder from './assets/hello-placeholder.png'
+import profile from './assets/profile.png'
 import './App.css'
 import './components/components.css'
 import Navbar from './components/Navbar'
@@ -18,9 +17,8 @@ function App() {
       <main id="home">
         <section id="center">
           <div className="hero">
-            <img src={heroImg} className="base" width="170" height="179" alt="" />
-            <img src={reactLogo} className="framework" alt="React logo" />
-            <img src={viteLogo} className="vite" alt="Vite logo" />
+            <img src={placeholder} className="base" alt="hero" />
+            <img src={profile} className="avatar" alt="profile" />
           </div>
           <div>
             <h1>Get started</h1>
@@ -51,13 +49,11 @@ function App() {
             <ul>
               <li>
                 <a href="https://vite.dev/" target="_blank">
-                  <img className="logo" src={viteLogo} alt="" />
                   Explore Vite
                 </a>
               </li>
               <li>
                 <a href="https://react.dev/" target="_blank">
-                  <img className="button-icon" src={reactLogo} alt="" />
                   Learn more
                 </a>
               </li>
