93dce8ae5fa928c7b2ff03b2607a73885bc39393
Jeremy Swagger <jswagger@agvance.net>
2026-07-28 13:19:39 -0500

    Fix inconsistent version references

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  assetsInclude: ['**/*.png', '**/*.PNG', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
   test: {
     environment: 'jsdom'
   }
